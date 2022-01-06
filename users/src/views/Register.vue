a7e947f7904c6ab55ec0d15d39b2b43878361473
Claudisnei Alves <claudisneibello@gmail.com>
2022-01-06 18:18:08 -0300

    View Registro de Usuários - Criado o componente Register.vue com o html de um formulário, um método data com as variáveis do fomulário, fizemos um databynd nos campos input utilizando v-model e um metodo register para imprimir no console o conteúdo dos campos input

---
+++ users/src/views/Register.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="content is-medium">
+        <h1 style="padding-top:40px">Registro de Usuário</h1>
+        <hr>
+        <div class="columns is-centered">
+            <div class="column is-one-third">
+                <div class="field">
+                    <label class="label" style="text-align:left">Nome</label>
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input is-success" type="text" placeholder="Nome do usuário" v-model="name">
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-user"></i>
+                        </span>
+                        <span class="icon is-small is-right">
+                            <i class="fas fa-check"></i>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label" style="text-align:left">Email</label>
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input is-danger" type="email" placeholder="Email do usuário" v-model="email">
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-envelope"></i>
+                        </span>
+                        <span class="icon is-small is-right">
+                            <i class="fas fa-exclamation-triangle"></i>
+                        </span>
+                    </div>
+
+                </div>
+
+                <div class="field">
+                    <label class="label" style="text-align:left">Password</label>
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input is-danger" type="password" placeholder="******" v-model="password">
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-lock"></i>
+                        </span>
+                        <span class="icon is-small is-right">
+                            <i class="fas fa-exclamation-triangle"></i>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <p class="control">
+                        <button class="button is-success is-medium is-fullwidth" @click="register">
+                            Login
+                        </button>
+                    </p>
+                </div>  
+
+            </div>    
+        </div>   
+    </div>    
+</template>
+<script>
+export default {
+    data(){
+        return{
+            name:'',
+            password:'',
+            email: ''
+        }  
+    },
+    methods:{
+        register(){
+            console.log(this.name);
+            console.log(this.email);
+            console.log(this.password);
+        }
+    }
+
+
+    
+}
+</script>
+<style>
+
+</style>
